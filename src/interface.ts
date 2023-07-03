87b9d4a216a79b1b48ea72cb1d286633ca7dcfe9
Eduardo Ban <eduardoban@gmail.com>
2023-07-03 11:37:16 -0300

    JSON ok

--- src/interface.ts
+++ src/interface.ts
@@ -5,8 +5,8 @@ export interface Welcome9 {
   export interface Centralizador {
     nom:  string;
     pasC: string;
-    bat:  number;
-    rssi: number;
+    bat?:  number;
+    rssi?: number;
     pMed: PMed[];
   }
   
@@ -14,22 +14,32 @@ export interface Welcome9 {
     id:   string;
     pas:  string;
     f:    string;
-    bat:  number;
-    rssi: number;
+    bat?:  number;
+    rssi?: number;
     daLo: DaLo[];
-  }
-  
-  export interface DaLo {  //Interface Data Logger
-   // f:  number;
-   
-    // A1: number;
-    // A2: number;
-    // A4: number;
-    // A9: number;
-  }
+   }
 
+
+export interface DaLo {
+  daLo: any;
+}
+
+  
+/*    export interface DaLo {  //Interface Data Logger
+    // f?:  number;  //Fecha formato UNIX EPOCH
+   A1: number;
+    A2: number;
+    A3: number;
+    A4: number;
+    A5: number;
+    A6: number;
+    A7: number;
+    A8: number;
+    A9: number; 
+  }  */
+ 
   // Create a generic interface:
-export interface DatosNuevo<T> {
+/* export interface DaLo<T> {
     date: Date;
     payload: T[];
-  }
+  } */
